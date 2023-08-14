bee0b05cc3a9d2d12bcd9c4e4b16721ddf32d9cc
tjalsgur0083 <tjalsgur0083@gmail.com>
2023-08-14 21:55:15 +0900

    server test success

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('ok');
+    res.send('test success');
     console.log('<http://localhost>: ${port}');
 });
 app.listen(port);
