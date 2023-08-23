d8ee7e94c4719df763fd68b2ce5d6d130e9629a1
tjalsgur0083 <tjalsgur0083@gmail.com>
2023-08-23 20:11:38 +0900

    Integration with mysql

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const promise_1 = __importDefault(require("mysql2/promise"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
+;
+const pool = promise_1.default.createPool({
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'HYeok!02584',
+    database: process.env.DB_NAME || 'user_data',
+    connectionLimit: 10,
+});
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
+});
+app.get('/user_table', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const connection = yield pool.getConnection();
+        const query = "SELECT * FROM user_table";
+        const [results] = yield connection.query(query);
+        connection.release();
+        res.json(results);
+    }
+    catch (err) {
+        console.error('유저 정보를 가져오기 실패:', err);
+        res.status(500).json({ error: '유저 정보 가져오기 실패' });
+    }
+}));
 app.get('/', (req, res) => {
-    res.send('test success');
+    res.send('running test');
     console.log('<http://localhost>: ${port}');
 });
-app.listen(port);
--- index.ts
+++ index.ts
@@ -1,11 +1,38 @@
 import express, {Express, Request, Response} from 'express';
+import mysql from 'mysql2/promise';
 
 const app: Express = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || '127.0.0.1',     // 데이터베이스 호스트
+  user: process.env.DB_USER || 'root',  // 데이터베이스 유저명
+  password: process.env.DB_PASSWORD || 'HYeok!02584',  // 데이터베이스 비밀번호
+  database: process.env.DB_NAME || 'user_data',   // 사용할 데이터베이스 이름
+  connectionLimit: 10,
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port ${port}');
+});
+
+app.get('/user_table', async (req: Request, res: Response) => {
+  try {
+    const connection = await pool.getConnection();
+
+    const query = "SELECT * FROM user_table";
+    const [results] = await connection.query(query);
+
+    connection.release();
+    res.json(results);
+  } catch (err) {
+    console.error('유저 정보를 가져오기 실패:', err);
+    res.status(500).json({ error: '유저 정보 가져오기 실패'});
+  }
+});
 
 app.get('/', (req: Request ,res: Response) => {
-  res.send('test success');
+  res.send('running test');
   console.log('<http://localhost>: ${port}');
 });
 
-app.listen(port);
