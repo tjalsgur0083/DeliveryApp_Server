0c2962b83bd31d8da67eeaa1127ce1ae4042df07
tjalsgur0083 <tjalsgur0083@gmail.com>
2023-09-11 10:07:01 +0900

    rewording code

--- router/user_table.js
+++ router/user_table.js
@@ -12,20 +12,40 @@ router.post('/user_login', (req, res) => {
     console.log("EMAIL : " + EMAIL);
     console.log("PASSWORD : " + PASSWORD);
     // 데이터베이스에서 사용자 정보를 조회하여 인증 수행
-    index_1.default.query('SELECT * FROM user_table WHERE EMAIL = ? AND PASSWORD = ?', [EMAIL, PASSWORD], (err, results) => {
+    index_1.default.query('SELECT * FROM user_table WHERE EMAIL = ?', [EMAIL], (err, results) => {
         if (err) {
             console.error('데이터베이스 오류:', err);
             res.status(500).json({ message: '서버 오류' });
             return;
         }
         if (results.length === 1) {
-            // 로그인 성공
+            //successful
             res.status(200).json({ message: '로그인 성공' });
-        }
-        else {
-            // 로그인 실패
-            res.status(401).json({ message: '로그인 실패' });
+            if (results[0].PASSWORD === PASSWORD) {
+                console.log('login success'); //
+            }
+            else { //
+                console.log('retry'); //
+            }
         }
     });
+    // pool.query(
+    //   'SELECT * FROM user_table WHERE EMAIL = ? AND PASSWORD = ?',
+    //   [EMAIL, PASSWORD],
+    //   (err, results) => {
+    //     if (err) {
+    //       console.error('데이터베이스 오류:', err);
+    //       res.status(500).json({ message: '서버 오류' });
+    //       return;
+    //     }
+    //     if (results.length === 1) {
+    //       // 로그인 성공
+    //       res.status(200).json({ message: '로그인 성공' });
+    //     } else {
+    //       // 로그인 실패
+    //       res.status(401).json({ message: '로그인 실패' });
+    //     }
+    //   }
+    // )
 });
 exports.default = router;
--- router/user_table.ts
+++ router/user_table.ts
@@ -9,25 +9,47 @@ router.post('/user_login', (req: Request, res: Response) => {
       console.log("EMAIL : " + EMAIL);
       console.log("PASSWORD : " + PASSWORD)
     // 데이터베이스에서 사용자 정보를 조회하여 인증 수행
+
     pool.query(
-      'SELECT * FROM user_table WHERE EMAIL = ? AND PASSWORD = ?',
-      [EMAIL, PASSWORD],
+      'SELECT * FROM user_table WHERE EMAIL = ?',
+      [EMAIL],
       (err, results) => {
         if (err) {
-          console.error('데이터베이스 오류:', err);
-          res.status(500).json({ message: '서버 오류' });
+          console.error('데이터베이스 오류:',err);
+          res.status(500).json({message: '서버 오류'});
           return;
         }
-  
+
         if (results.length === 1) {
-          // 로그인 성공
-          res.status(200).json({ message: '로그인 성공' });
-        } else {
-          // 로그인 실패
-          res.status(401).json({ message: '로그인 실패' });
+          //successful
+          res.status(200).json({ message: '로그인 성공'});
+          if (results[0].PASSWORD === PASSWORD) {
+            console.log('login success'); //
+          } else {  //
+            console.log('retry'); //
+          }
         }
       }
     )
+    // pool.query(
+    //   'SELECT * FROM user_table WHERE EMAIL = ? AND PASSWORD = ?',
+    //   [EMAIL, PASSWORD],
+    //   (err, results) => {
+    //     if (err) {
+    //       console.error('데이터베이스 오류:', err);
+    //       res.status(500).json({ message: '서버 오류' });
+    //       return;
+    //     }
+  
+    //     if (results.length === 1) {
+    //       // 로그인 성공
+    //       res.status(200).json({ message: '로그인 성공' });
+    //     } else {
+    //       // 로그인 실패
+    //       res.status(401).json({ message: '로그인 실패' });
+    //     }
+    //   }
+    // )
 });
 
 export default router;
